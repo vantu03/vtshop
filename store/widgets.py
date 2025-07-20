2f794bdf0865d1eff292a20891a3d82b4eeb8e2e
vantu03 <concaoheo203@gmail.com>
2025-07-20 19:33:25 +0700

    commit

--- store/widgets.py
+++ store/widgets.py
@@ -21,7 +21,7 @@ class GridSelectModalWidget(forms.CheckboxSelectMultiple):
         trigger = format_html(
             '''
             <button type="button" class="btn btn-outline-primary mb-2" data-bs-toggle="modal" data-bs-target="#{}">
-                <i class="bi bi-list-check me-1"></i> Chọn mục
+                <i class="bi bi-list-check me-1"></i> Select
             </button>
             ''',
             modal_id
@@ -33,7 +33,7 @@ class GridSelectModalWidget(forms.CheckboxSelectMultiple):
               <div class="modal-dialog modal-xl modal-dialog-scrollable">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="{}Label"><i class="bi bi-list-check me-1"></i>Chọn mục</h5>
+                    <h5 class="modal-title" id="{}Label"><i class="bi bi-list-check me-1"></i>Select</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
@@ -44,7 +44,7 @@ class GridSelectModalWidget(forms.CheckboxSelectMultiple):
         modal_footer = '''
                   </div>
                   <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">X</button>
                   </div>
                 </div>
               </div>
@@ -60,10 +60,9 @@ class GridSelectModalWidget(forms.CheckboxSelectMultiple):
             item_html = format_html(
                 '''
                 <div class="col-6 col-md-4 col-lg-3">
-                    <label class="grid-option position-relative d-block border rounded p-2 bg-light text-start h-100">
-                        <input type="checkbox" name="{name}" value="{value}" class="form-check-input position-absolute top-0 start-0 m-2" {checked}>
+                    <label class="grid-option d-block border rounded p-2 bg-light text-start h-100">
+                        <input type="checkbox" name="{name}" value="{value}" class="form-check-input" {checked}>
                         {content}
-                        <i class="bi bi-check-circle-fill text-primary position-absolute top-0 end-0 m-2" style="font-size: 1.2rem; {show_check}"></i>
                     </label>
                 </div>
                 ''',
@@ -71,7 +70,6 @@ class GridSelectModalWidget(forms.CheckboxSelectMultiple):
                 value=obj_id,
                 checked='checked' if selected else '',
                 content='Chưa có',
-                show_check='' if selected else 'display: none;'
             )
             grid_items.append(item_html)
 
